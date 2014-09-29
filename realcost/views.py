39cafeb1c49d131e7804e87197a2997e75bdc13d
Ryan Currah <ryan.currah@td.com>
2014-09-28 21:35:50 -0400

    added real interest rate formula

--- realcost/views.py
+++ realcost/views.py
@@ -25,6 +25,8 @@ def home(request):
             bank_total_cost = round((net_price - rebate) * ((1 + bank_borrowing_rate/bank_compounding_period)**(bank_compounding_period*TERM_YEARS)), 2)
             dealership_total_cost = round(net_price * ((1 + dealership_borrowing_rate/dealership_compounding_period)**(bank_compounding_period*TERM_YEARS)), 2)
 
+            real_interest_rate = round(dealership_compounding_period * ((Decimal(dealership_total_cost)/(net_price - rebate)) ** (1 / (dealership_compounding_period * TERM_YEARS))), 2)
+
             bank_total_interest_dollars = round(Decimal(bank_total_cost) - (net_price - rebate), 2)
             dealership_total_interest_dollars = round(Decimal(dealership_total_cost) - net_price, 2)
 
@@ -32,7 +34,8 @@ def home(request):
                                                  'bank_total_cost': bank_total_cost,
                                                  'dealership_total_cost': dealership_total_cost,
                                                  'bank_total_interest_dollars': bank_total_interest_dollars,
-                                                 'dealership_total_interest_dollars': dealership_total_interest_dollars})
+                                                 'dealership_total_interest_dollars': dealership_total_interest_dollars,
+                                                 'real_interest_rate': real_interest_rate})
     else:
         form = RealCostForm()
     return render(request, 'home.html', {'form': form})
