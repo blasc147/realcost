9111f3b708f6cd0b17c23f30140bbd9236a658b1
Brian Stock <bstock92@gmail.com>
2014-09-30 22:49:09 -0400

    Amortization and housekeeping
    Amortization implemented
     - Calculate monthly payment, determine total cost, and determine
       interest cost, normalized with respect to the price less rebate.
     - Newton root-finding algorithm used to solve amortization
       equation with respect to i (interest rate)
     - WARNING: Newton's Methid is not guaranteed to converge on a
    	    root. Use bounded method if problems occur.
     - will require additional work to support variable compound rates
     - Asking for interest rate in terms of APR will automatically
       account for compound interest.
    
    Output format modified
     - % sign for APR removed
     - if units belong in table cells, include it for dollar amounts
       as well, using string formatting syntax
     - string formatting syntax for dollar amounts to force exactly 2
       decimal places
     - calculated Dealership APR precision increased to up to 4
       decimal places
    
    Input form modified
     - variable compound rates not required/supported with
       amortization implementation
     - removed dealer and bank compound period inputs
     - TODO: place upper limit on inputs to prevent overflows
    
    Code
     - views.py heavily commented
     - variable names changed
       - e.g. interest_rate -> apr

--- realcost/forms.py
+++ realcost/forms.py
@@ -14,25 +14,22 @@ COMPOUND_PERIOD = (
 class RealCostForm(forms.Form):
     net_price = forms.DecimalField(label='Vehicle net price',
                                    decimal_places=2,
-                                   help_text=u"Net price includes applicable government sales taxes, "
+                                   help_text=u"Net price is the amount to be borrowed "
+                                             u"and includes applicable government sales taxes, "
                                              u"licensing, fuel fill charge, insurance, dealer PDI, PPSA, "
                                              u"administration fees, and any other applicable environmental "
-                                             u"charges/fees and taxes.")
+                                             u"charges/fees and taxes, minus any down payment.")
     rebate = forms.DecimalField(label='Cash rebate',
                                 decimal_places=2,
                                 help_text=u"The dollar amount of the rebate when using cash or "
-                                          u"non dealership loan.")
-    bank_borrowing_rate = forms.DecimalField(decimal_places=2,
+                                          u"non-dealership loan.")
+    bank_borrowing_rate = forms.DecimalField(label='Bank APR',
+                                             decimal_places=2,
                                              help_text=u"The interest rate the bank is offering for a loan.")
-    bank_compounding_period = forms.ChoiceField(choices=COMPOUND_PERIOD,
-                                                help_text=u"How often the bank compounds the interest "
-                                                          u"in a year.")
-    dealership_borrowing_rate = forms.DecimalField(decimal_places=2,
+    dealership_borrowing_rate = forms.DecimalField(label='Dealership APR',
+                                                   decimal_places=2,
                                                    help_text=u"The interest rate the dealership is offering "
                                                              u"for a loan.")
-    dealership_compounding_period = forms.ChoiceField(choices=COMPOUND_PERIOD,
-                                                      help_text=u"How often the dealership compounds the interest "
-                                                                u"in a year.")
     months_borrowed = forms.ChoiceField(choices=((x, x) for x in range(1, 85)))
 
     def __init__(self, *args, **kwargs):
@@ -46,10 +43,8 @@ class RealCostForm(forms.Form):
                     Div(
                         Div(PrependedText('net_price', '$', placeholder='Net price'), css_class="col-lg-12"),
                         Div(PrependedText('rebate', '$', placeholder='Cash rebate'), css_class="col-lg-12"),
-                        Div(PrependedText('bank_borrowing_rate', '%', placeholder='Bank borrowing rate'), css_class="col-lg-8"),
-                        Div(Field('bank_compounding_period'), css_class="col-lg-4"),
-                        Div(PrependedText('dealership_borrowing_rate', '%', placeholder='Dealership borrowing rate'), css_class="col-lg-8"),
-                        Div(Field('dealership_compounding_period'), css_class="col-lg-4"),
+                        Div(PrependedText('bank_borrowing_rate', '%', placeholder='Bank borrowing rate'), css_class="col-lg-4"),
+                        Div(PrependedText('dealership_borrowing_rate', '%', placeholder='Dealership borrowing rate'), css_class="col-lg-4"),
                         Div(Field('months_borrowed'), css_class="col-lg-12"),
                         css_class="row"
                     ),
--- realcost/views.py
+++ realcost/views.py
@@ -1,43 +1,84 @@
 from .forms import RealCostForm
 from django.shortcuts import render
 from decimal import Decimal
+from scipy.optimize import newton
+
+
+def rootfinding_amortization(i, P, n, A):
+    return P*(i + (i/((1+i)**n - 1))) - A
+
+
+def payment(principal, apr, number_of_periods):
+    if apr == 0:
+        periodic_payment = principal/number_of_periods
+    else:
+        monthly_interest_rate = apr/12
+        periodic_payment = principal*(monthly_interest_rate + (monthly_interest_rate/((1+monthly_interest_rate)**number_of_periods - 1)))
+    return round(periodic_payment, 2)
+
 
 def home(request):
     if request.method == 'POST':
         form = RealCostForm(request.POST)
         if form.is_valid():
-            # get form values
+
+            # Gather and sanitize inputs.
             net_price = form.cleaned_data['net_price']
-
             rebate = form.cleaned_data['rebate']
+            bank_apr = form.cleaned_data['bank_borrowing_rate'] / Decimal(100)
+            dealership_apr = form.cleaned_data['dealership_borrowing_rate'] / Decimal(100)
+            loan_periods = int(form.cleaned_data['months_borrowed'])
 
-            bank_borrowing_rate = form.cleaned_data['bank_borrowing_rate'] / Decimal(100)
-            bank_compounding_period = Decimal(form.cleaned_data['bank_compounding_period'])
 
-            dealership_borrowing_rate = form.cleaned_data['dealership_borrowing_rate'] / Decimal(100)
-            dealership_compounding_period = Decimal(form.cleaned_data['dealership_compounding_period'])
+            # Calculated Values
+            bank_principle = net_price - rebate
 
-            months_borrowed = form.cleaned_data['months_borrowed']
 
-            # calc values
-            TERM_YEARS = Decimal(months_borrowed)/Decimal(12)
+            # Monthly Payment Calculations
+            bank_monthly_payment = payment(bank_principle, bank_apr, loan_periods)
+            dealership_monthly_payment = payment(net_price, dealership_apr, loan_periods)
 
-            bank_total_cost = round((net_price - rebate) * ((1 + bank_borrowing_rate/bank_compounding_period)**(bank_compounding_period*TERM_YEARS)), 2)
-            dealership_total_cost = round(net_price * ((1 + dealership_borrowing_rate/dealership_compounding_period)**(bank_compounding_period*TERM_YEARS)), 2)
 
-            real_bank_interest_rate = round(bank_borrowing_rate * 100, 2)
-            real_dealership_interest_rate = round((dealership_compounding_period * ((Decimal(dealership_total_cost)/(net_price - rebate)) ** (1 / (dealership_compounding_period * TERM_YEARS)) - 1)) + (dealership_borrowing_rate * 100), 2)
+            # Total Cost Calculations
+            bank_total_cost = bank_monthly_payment * loan_periods
+            dealership_total_cost = dealership_monthly_payment * loan_periods
 
-            bank_total_interest_dollars = round(Decimal(bank_total_cost) - (net_price - rebate), 2)
-            dealership_total_interest_dollars = round(Decimal(dealership_total_cost) - (net_price - rebate), 2)
 
+            # Effective Interest Cost Calculations
+            # Cost of interest is normalized with respect to the rebated price.
+            bank_total_interest_dollars = round(Decimal(bank_total_cost) - bank_principle, 2)
+            dealership_total_interest_dollars = round(Decimal(dealership_total_cost) - bank_principle, 2)
+
+
+            # Effective APR Calculation
+            bank_apr = round(bank_apr * 100, 4)
+            # Effective Dealership APR is calculated using Newton root-finding Method with initial approximation of the advertised APR
+            # The root-finding library only works with floats.
+            if dealership_apr == 0:
+                # Avoid divide by zero error in root-finding method.
+                # Approximate 0 for initial guess.
+                initial_guess = 0.000000001
+            else:
+                # Use dealership_apr as initial guess for root-finding.
+                # In most real-world cases, this should reasonably approximate the effective APR.
+                initial_guess = dealership_apr
+
+            # WARNING: Newton's root-finding method is NOT guaranteed to converge! In some unlikely cases, the method
+            #          may fail to solve the equation. Use a bounded root-finding method if problems occur.
+            effective_dealership_apr = round(12 * 100 * newton(rootfinding_amortization, float(initial_guess), args=(float(bank_principle), float(loan_periods), float(dealership_monthly_payment))), 4)
+
+
+            # Return values to HTML page.
+            # Dollar amounts are formatted to exactly two decimal places.
             return render(request, 'home.html', {'form': form,
-                                                 'bank_total_cost': bank_total_cost,
-                                                 'dealership_total_cost': dealership_total_cost,
-                                                 'bank_total_interest_dollars': bank_total_interest_dollars,
-                                                 'dealership_total_interest_dollars': dealership_total_interest_dollars,
-                                                 'real_dealership_interest_rate': real_dealership_interest_rate,
-                                                 'real_bank_interest_rate': real_bank_interest_rate})
+                                                 'bank_total_cost': '%.2f' % bank_total_cost,
+                                                 'dealership_total_cost': '%.2f' % dealership_total_cost,
+                                                 'bank_total_interest_dollars': '%.2f' % bank_total_interest_dollars,
+                                                 'dealership_total_interest_dollars': '%.2f' % dealership_total_interest_dollars,
+                                                 'bank_monthly_payment': '%.2f' % bank_monthly_payment,
+                                                 'dealership_monthly_payment': '%.2f' % dealership_monthly_payment,
+                                                 'real_dealership_interest_rate': effective_dealership_apr,
+                                                 'real_bank_interest_rate': bank_apr})
     else:
         form = RealCostForm()
     return render(request, 'home.html', {'form': form})
