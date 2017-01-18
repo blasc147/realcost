a0336e04e6a3af264788804bafdb7cef30400f75
Brian Stock <bstock92@gmail.com>
2017-01-18 01:59:55 +0000

    upgraded view to from function-based to class-based

--- realcost/urls.py
+++ realcost/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from .views import *
 from realcost import views as realcost_views
+from realcost.views import RealCostView
 
 urlpatterns = [
-    url(r'^$', realcost_views.home, name='home'),
+    #url(r'^$', realcost_views.home, name='home'),
+    url(r'^$', RealCostView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- realcost/views.py
+++ realcost/views.py
@@ -1,77 +1,87 @@
 from .forms import RealCostForm
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 from decimal import Decimal
 
-
-def rootfinding_amortization(i, P, n, A):
-    return P*(i + (i/((1+i)**n - 1))) - A
-
-
-def payment(principal, apr, number_of_periods):
-    if apr == 0:
-        periodic_payment = principal/number_of_periods
-    else:
-        monthly_interest_rate = apr/12
-        periodic_payment = principal*(monthly_interest_rate + (monthly_interest_rate/((1+monthly_interest_rate)**number_of_periods - 1)))
-    return periodic_payment
+class RealCostView(View):
+    def rootfinding_amortization(self, i, P, n, A):
+        return P*(i + (i/((1+i)**n - 1))) - A
 
 
-def home(request):
-    if request.method == 'POST':
-        form = RealCostForm(request.POST)
-        if form.is_valid():
-            # Gather and sanitize inputs
-            net_price = form.cleaned_data['net_price']
-            rebate = form.cleaned_data['rebate']
-            bank_apr = form.cleaned_data['bank_borrowing_rate'] / Decimal(100)
-            dealership_apr = form.cleaned_data['dealership_borrowing_rate'] / Decimal(100)
-            loan_periods = int(form.cleaned_data['months_borrowed'])
-
-            # Calculated Values
-            bank_principle = net_price - rebate
+    def payment(self, principal, apr, number_of_periods):
+        if apr == 0:
+            periodic_payment = principal/number_of_periods
+        else:
+            monthly_interest_rate = apr/12
+            periodic_payment = principal*(monthly_interest_rate + (monthly_interest_rate/((1+monthly_interest_rate)**number_of_periods - 1)))
+        return periodic_payment
 
 
-            # Monthly Payment Calculations
-            bank_monthly_payment = payment(bank_principle, bank_apr, loan_periods)
-            dealership_monthly_payment = payment(net_price, dealership_apr, loan_periods)
+    def home(self, request):
+        if request.method == 'POST':
+            form = RealCostForm(request.POST)
+            if form.is_valid():
+                # Gather and sanitize inputs
+                net_price = form.cleaned_data['net_price']
+                rebate = form.cleaned_data['rebate']
+                bank_apr = form.cleaned_data['bank_borrowing_rate'] / Decimal(100)
+                dealership_apr = form.cleaned_data['dealership_borrowing_rate'] / Decimal(100)
+                loan_periods = int(form.cleaned_data['months_borrowed'])
+
+                # Calculated Values
+                bank_principle = net_price - rebate
 
 
-            # Total Cost Calculations
-            bank_total_cost = bank_monthly_payment * loan_periods
-            dealership_total_cost = dealership_monthly_payment * loan_periods
+                # Monthly Payment Calculations
+                bank_monthly_payment = self.payment(bank_principle, bank_apr, loan_periods)
+                dealership_monthly_payment = self.payment(net_price, dealership_apr, loan_periods)
 
-            # Effective Interest Cost Calculations
-            # Cost of interest is normalized with respect to the rebated price
-            bank_total_interest_dollars = round(Decimal(bank_total_cost) - bank_principle, 2)
-            dealership_total_interest_dollars = round(Decimal(dealership_total_cost) - bank_principle, 2)
 
-            # Effective APR Calculation
-            bank_apr = round(bank_apr * 100, 4)
+                # Total Cost Calculations
+                bank_total_cost = bank_monthly_payment * loan_periods
+                dealership_total_cost = dealership_monthly_payment * loan_periods
 
-            # Effective Dealership APR is calculated using Newton root-finding Method with initial approximation of the advertised APR
-            # The root-finding library only works with floats
-            if dealership_apr == 0:
-                # Avoid divide by zero error in root-finding method
-                # Approximate 0 for initial guess.
-                initial_guess = 0.000000001
-            else:
-                # Use dealership_apr as initial guess for root-finding
-                # In most real-world cases, this should reasonably approximate the effective APR
-                initial_guess = dealership_apr
+                # Effective Interest Cost Calculations
+                # Cost of interest is normalized with respect to the rebated price
+                bank_total_interest_dollars = round(Decimal(bank_total_cost) - bank_principle, 2)
+                dealership_total_interest_dollars = round(Decimal(dealership_total_cost) - bank_principle, 2)
 
-            effective_dealership_apr = round(12 * 100 * (float(initial_guess) * float(bank_principle) * float(loan_periods) * float(dealership_monthly_payment)))
+                # Effective APR Calculation
+                bank_apr = round(bank_apr * 100, 4)
 
-            # Return values to HTML page
-            # Dollar amounts are formatted to exactly two decimal places
-            return render(request, 'home.html', {'form': form,
-                                                 'bank_total_cost': Decimal('%.2f' % bank_total_cost),
-                                                 'dealership_total_cost': Decimal('%.2f' % dealership_total_cost),
-                                                 'bank_total_interest_dollars': Decimal('%.2f' % bank_total_interest_dollars),
-                                                 'dealership_total_interest_dollars': Decimal('%.2f' % dealership_total_interest_dollars),
-                                                 'bank_monthly_payment': Decimal('%.2f' % bank_monthly_payment),
-                                                 'dealership_monthly_payment': Decimal('%.2f' % dealership_monthly_payment),
-                                                 'real_dealership_interest_rate': effective_dealership_apr,
-                                                 'real_bank_interest_rate': bank_apr})
-    else:
-        form = RealCostForm()
-    return render(request, 'home.html', {'form': form})
+                # Effective Dealership APR is calculated using Newton root-finding Method with initial approximation of the advertised APR
+                # The root-finding library only works with floats
+                if dealership_apr == 0:
+                    # Avoid divide by zero error in root-finding method
+                    # Approximate 0 for initial guess.
+                    initial_guess = 0.000000001
+                else:
+                    # Use dealership_apr as initial guess for root-finding
+                    # In most real-world cases, this should reasonably approximate the effective APR
+                    initial_guess = dealership_apr
+
+                effective_dealership_apr = round(12 * 100 * (float(initial_guess) * float(bank_principle) * float(loan_periods) * float(dealership_monthly_payment)))
+
+                # Return values to HTML page
+                # Dollar amounts are formatted to exactly two decimal places
+                return render(request, 'home.html', {'form': form,
+                                                     'bank_total_cost': Decimal('%.2f' % bank_total_cost),
+                                                     'dealership_total_cost': Decimal('%.2f' % dealership_total_cost),
+                                                     'bank_total_interest_dollars': Decimal('%.2f' % bank_total_interest_dollars),
+                                                     'dealership_total_interest_dollars': Decimal('%.2f' % dealership_total_interest_dollars),
+                                                     'bank_monthly_payment': Decimal('%.2f' % bank_monthly_payment),
+                                                     'dealership_monthly_payment': Decimal('%.2f' % dealership_monthly_payment),
+                                                     'real_dealership_interest_rate': effective_dealership_apr,
+                                                     'real_bank_interest_rate': bank_apr})
+        else:
+            form = RealCostForm()
+        return render(request, 'home.html', {'form': form})
+    
+
+    def get(self, request):
+        return self.home(request)
+
+
+    def post(self, request):
+        return self.home(request)
